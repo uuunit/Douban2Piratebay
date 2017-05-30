b9cd7f4c1ae04a75e73160e1e938e5329215f2c0
camac <camacs@gmail.com>
2017-05-30 10:37:04 +0800

    Update Douban2Rargb.meta.js

--- Douban2Rargb.meta.js
+++ Douban2Rargb.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Douban2Piratebay
-// @namespace    https://github.com/bitdust/Douban2Piratebay/
-// @version      0.5
-// @description  And direct link to piratebay from douban movie page.
+// @name         Douban2Rarbg
+// @namespace    https://github.com/uuunit/Douban2Rarbg/
+// @version      0.1
+// @description  And direct link to Rargb from douban movie page.
 // @author       bitdust
 // @match        https://movie.douban.com/subject/*
-// @updateURL    https://raw.githubusercontent.com/bitdust/Douban2Piratebay/master/Douban2Piratebay.meta.js
-// @downloadURL  https://raw.githubusercontent.com/bitdust/Douban2Piratebay/master/Douban2Piratebay.user.js
+// @updateURL    https://raw.githubusercontent.com/uuunit/Douban2Rarbg/blob/master/Douban2Rargb.meta.js
+// @downloadURL  https://raw.githubusercontent.com/uuunit/Douban2Rarbg/blob/master/Douban2Rargb.user.js
 // @grant        none
 // ==/UserScript==
